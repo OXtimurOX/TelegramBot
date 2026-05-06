3179a8dec4c8188be2aeb7da56e8f0229aa4f2b6
OXtimurOX <ilnazbahtiev@gmail.com>
2026-05-06 09:09:32 +0300

    Pause higher

--- main.go
+++ main.go
@@ -101,8 +101,8 @@ func main() {
 				time.Sleep(10 * time.Second)
 			}
 
-			fmt.Println("⏸️ Ждем 1 минуту...")
-			time.Sleep(1 * time.Minute)
+			fmt.Println("⏸️ Ждем 10 минут...")
+			time.Sleep(10 * time.Minute)
 		}
 	}
 }
